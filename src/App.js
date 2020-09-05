6b4f88b1851d2560592c0b4aaf0970301767c185
Malaika <malaikafernandes25@gmail.com>
2020-09-05 14:16:35 +0530

    weather updates

--- src/App.js
+++ src/App.js
@@ -1,21 +1,42 @@
 import React from 'react';
 import './App.css';
 import { useState, useEffect } from 'react';
+import Weather from './components/weather'
 
-const Weather = () => {
+const App = () => {
   const ACCESS_KEY = "9ef48c0705499069b88c8af0bf120522";
+
+  const [weatherData, setWeatherData] = useState({});
   useEffect(()=>{
     getData();
   },[])
 
   const getData = async()=>{
     const response = await fetch (`http://api.weatherstack.com/current?access_key=${ACCESS_KEY}&query=New York`);
-    const data = response.json();
-    console.log(data);
+    const data = await response.json();
+    console.log(data.location);
+    setWeatherData( Object.values(data));
+   
+   
   }
+
+ 
   return (
-   <h1> Hello React </h1>
+    <div className="App">
+      <h1> Weather App  </h1>
+      <form>
+        <input type="text" />
+        <button type="submit" > Submit </button>
+      </form>
+
+      {/* {weatherData.map(weather => (
+        <Weather />
+          // <Weather temp="weather.current.temperature" status="Clear" humidity="20" time="12" country="India"/>
+      ))} */}
+    
+    </div>
+   
   );
 }
 
-export default Weather;
+export default App;
---
+++ src/components/weather.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+// const Weather = ({temp,status,humidity,time,country}) => {
+//     return(
+//         <div className="weather-wrapper"> 
+//             <h2> {country}</h2>
+//             <h4> {temp}</h4>
+//             <p> {status}</p>
+//             <p> {humidity}</p>
+//             <p> {time} </p>
+//         </div>
+        
+//     );
+// }
+const Weather = () => {
+    return(
+        <div className="weather-wrapper"> 
+            <h2> country</h2>
+            <h4> temp</h4>
+            <p> status</p>
+            <p> humidity</p>
+            <p> time </p>
+        </div>
+        
+    );
+}
+
+export default Weather;
