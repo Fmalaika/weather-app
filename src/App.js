260054986fc8f76c45796f4b5999091e12bd7666
Malaika <malaikafernandes25@gmail.com>
2020-09-05 16:16:33 +0530

    search functionality

--- src/App.js
+++ src/App.js
@@ -7,32 +7,49 @@ const App = () => {
   const ACCESS_KEY = "9ef48c0705499069b88c8af0bf120522";
 
   const [weatherData, setWeatherData] = useState({});
+  const [search, setSearch] = useState('');
+  const [query, setQuery] = useState('India');
   useEffect(()=>{
     getData();
-  },[])
+  },[query])
 
   const getData = async()=>{
-    const response = await fetch (`http://api.weatherstack.com/current?access_key=${ACCESS_KEY}&query=New York`);
+    const response = await fetch (`http://api.weatherstack.com/current?access_key=${ACCESS_KEY}&query=${query}`);
     const data = await response.json();
-    console.log(data.location);
-    setWeatherData( Object.values(data));
+    // console.log(data.location);
+    setWeatherData( data);
    
    
   }
 
- 
+  const updateSearch = (e) => {
+    setSearch(e.target.value);
+    console.log(e.target.value)
+  }
+
+  const getSearch = (e) => {
+    e.preventDefault();
+    setQuery(search)
+
+
+  }
+
+ let temp = weatherData.current ? weatherData.current.temperature : null;
+ let status = weatherData.current ? weatherData.current.weather_descriptions : null;
+ let humidity = weatherData.current ? weatherData.current.humidity : null;
+ let time = weatherData.current ? weatherData.location.localtime : null;
+ let country = weatherData.current ? weatherData.location.country : null;
+
   return (
     <div className="App">
       <h1> Weather App  </h1>
-      <form>
-        <input type="text" />
-        <button type="submit" > Submit </button>
+      <form onSubmit={getSearch}>
+        <input type="text" value={search} onChange={updateSearch} />
+        <button type="submit"> Submit </button>
       </form>
-
-      {/* {weatherData.map(weather => (
-        <Weather />
-          // <Weather temp="weather.current.temperature" status="Clear" humidity="20" time="12" country="India"/>
-      ))} */}
+      
+         <Weather temp={temp} status={status} humidity={humidity} time={time} country={country}/>
+ 
     
     </div>
    
--- src/components/weather.js
+++ src/components/weather.js
@@ -1,28 +1,17 @@
 import React from 'react';
 
-// const Weather = ({temp,status,humidity,time,country}) => {
-//     return(
-//         <div className="weather-wrapper"> 
-//             <h2> {country}</h2>
-//             <h4> {temp}</h4>
-//             <p> {status}</p>
-//             <p> {humidity}</p>
-//             <p> {time} </p>
-//         </div>
-        
-//     );
-// }
-const Weather = () => {
+const Weather = ({temp,status,humidity,time,country}) => {
     return(
         <div className="weather-wrapper"> 
-            <h2> country</h2>
-            <h4> temp</h4>
-            <p> status</p>
-            <p> humidity</p>
-            <p> time </p>
+            <h2> {country}</h2>
+            <h4> {temp}</h4>
+            <p> {status}</p>
+            <p> {humidity}</p>
+            <p> {time} </p>
         </div>
         
     );
 }
+
 
 export default Weather;
